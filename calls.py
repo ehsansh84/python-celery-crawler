4dbeb0d0b18941e4a09511f0066d04ddff4cddb6
ehsan <ehsan.shirzadi@gmail.com>
2023-02-05 14:20:54 +0330

    added: calls.py

---
+++ calls.py
@@ -0,0 +1,35 @@
+from crawler import crawl, extract_info
+from celery import Celery, chain
+app = Celery('calls')
+
+print('Starting...')
+urls = [
+    'https://quotes.toscrape.com',
+]
+
+selectors = {
+    'text': {
+        'path': 'span:nth-child(1)',
+        'attr': 'text'
+    },
+    'author': {
+        'path': '.author',
+        'attr': 'text'
+    },
+    'author_url': {
+        'path': 'span:nth-child(2)>a',
+        'attr': 'href'
+    }
+}
+
+
+
+chain(
+    app.signature("crawl", args=[urls[0], 'div.quote']).set(queue="crawl"),
+    app.signature("extract", args=[selectors]).set(queue="extract")
+).apply_async()
+
+# data = crawl(url=urls[0], selector='div.quote')
+print('Done!')
+
+# print(extract_info(data, selectors))
